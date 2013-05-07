8bdf7fa5a03d6e5d78bc9ea882ba18f698630ead
Axel de Sablet <contact@axel-desablet.fr>
2013-05-07 22:41:40 +0200

    Add code to call iText to read and write the PDF files

--- src/deprotect/DeprotectMain.java
+++ src/deprotect/DeprotectMain.java
@@ -1,10 +1,12 @@
 package deprotect;
 
 import com.google.common.io.Files;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfEncryptor;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -29,6 +31,34 @@ public class DeprotectMain {
 		final File outputFile = new File(filenameWithoutExtension);
 		
 		
+		final PdfReader reader;
+		try {
+			reader = new PdfReader(inputFile.getPath());
+		} catch (final IOException ex) {
+			Logger.getLogger(DeprotectMain.class.getName()).log(
+					Level.SEVERE,
+					"Unexpected IOException : iText failed to read the input file",
+					ex);
+			throw new RuntimeException(ex);
+		}
+		
+		try {
+			final FileOutputStream outputStream = new FileOutputStream(outputFile);
+		
+			PdfEncryptor.encrypt(reader, outputStream, null,
+					null, PdfWriter.AllowAssembly | PdfWriter.AllowCopy
+					| PdfWriter.AllowDegradedPrinting | PdfWriter.AllowFillIn
+					| PdfWriter.AllowModifyAnnotations | PdfWriter.AllowModifyContents
+					| PdfWriter.AllowPrinting | PdfWriter.AllowScreenReaders, false);
+		} catch (final IOException ex) {
+			Logger.getLogger(DeprotectMain.class.getName()).log(
+					Level.SEVERE,
+					"Unexpected IOException : the ouput file could not be created",
+					ex);
+			throw new RuntimeException(ex);
+		} catch (DocumentException ex) {
+			Logger.getLogger(DeprotectMain.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 	
 }
